bcc26d229db8710c5aeaa934f233e531f8820a18
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2025-11-17 12:12:19 -0300

    Update CalendarView.jsx

--- src/components/Calendar/CalendarView.jsx
+++ src/components/Calendar/CalendarView.jsx
@@ -1,3 +1,8 @@
 export default function CalendarView() {
-  return <h2>Calendário</h2>;
+  return (
+    <div>
+      <h2>Calendário</h2>
+      <p>Visualização mensal/semanal/dia.</p>
+    </div>
+  );
 }
