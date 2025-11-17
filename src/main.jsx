34f44e4047647d05a3c226634cbde1f5b5f46f0f
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2025-11-17 07:50:47 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/globals.css";
-+ import { UserProvider } from "./context/UserContext";
+import "./styles/layout.css";
+import { UserProvider } from "./context/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-+   <UserProvider>
+    <UserProvider>
       <App />
-+   </UserProvider>
+    </UserProvider>
   </React.StrictMode>
 );
