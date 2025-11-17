c5f87ba3c811abd760396ccd477909d6a68e33bf
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2025-11-17 07:50:13 -0300

    Update config.js

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -1,12 +1,19 @@
+// src/services/firebase/config.js
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyBcQi5nToMOGVDBWprhhOY0NSJX4qE100w",
+  authDomain: "portal-relevo.firebaseapp.com",
+  projectId: "portal-relevo",
+  storageBucket: "portal-relevo.firebasestorage.app",
+  messagingSenderId: "182759626683",
+  appId: "1:182759626683:web:2dde2eeef910d4c288569e",
+  measurementId: "G-W8TTP3D3YQ"
 };
 
-export const app = initializeApp(firebaseConfig);
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta Firestore para uso nos serviços
+export const db = getFirestore(app);
