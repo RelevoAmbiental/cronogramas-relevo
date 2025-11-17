3dbaacf0650e85c9911b7ed130b1e12aaa7d72bc
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2025-11-17 12:11:19 -0300

    Update Dashboard.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,3 +1,8 @@
 export default function Dashboard() {
-  return <h2>Dashboard do Cronograma</h2>;
+  return (
+    <div>
+      <h2>Dashboard do Cronograma</h2>
+      <p>Visão geral dos projetos e tarefas.</p>
+    </div>
+  );
 }
