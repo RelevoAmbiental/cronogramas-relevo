eecc0965298f3761f8e9ca564829c2d4610f2cd8
Relevo Consultoria Ambiental <samuel@relevo.eco.br>
2025-11-17 12:42:21 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./styles/globals.css";
-import "./styles/layout.css";
+import App from "./App";
+
 import { UserProvider } from "./context/UserContext";
+import { CronogramaProvider } from "./context/CronogramaContext";
+
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
-      <App />
+      <CronogramaProvider>
+        <App />
+      </CronogramaProvider>
     </UserProvider>
   </React.StrictMode>
 );
